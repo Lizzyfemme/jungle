ac3ea90f33f6a0442334add3aac6d5583decee8b
Lizzyfemme <lizzy_swann@hotmail.com>
2019-11-14 20:54:07 +0000

    Nav bar edited

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,9 +1,7 @@
 class Admin::CategoriesController < ApplicationController
   http_basic_authenticate_with name: ENV["HTTP_ADMIN_USER"],
                                password: ENV["HTTP_ADMIN_PASSWORD"]
-  def show
-   
-  end
+
   def index
     @category = Category.order(:name).all
   end
