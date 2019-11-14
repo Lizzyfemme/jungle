0653dad6edd474e97520e0df56fb21ab93e8e559
Lizzyfemme <lizzy_swann@hotmail.com>
2019-11-14 00:06:47 +0000

    Password authentification added

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,6 @@
 class Admin::DashboardController < ApplicationController
+  http_basic_authenticate_with name: ENV["HTTP_ADMIN_USER"],
+                             password: ENV["HTTP_ADMIN_PASSWORD"]
   def show
   end
 end
--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,4 +1,6 @@
 class Admin::ProductsController < ApplicationController
+  http_basic_authenticate_with name: ENV["HTTP_ADMIN_USER"],
+                             password: ENV["HTTP_ADMIN_PASSWORD"]
 
   def index
     @products = Product.order(id: :desc).all
