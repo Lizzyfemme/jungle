8cd61bfa701b6585fb4def517e56e024dbfc01b1
Lizzyfemme <lizzy_swann@hotmail.com>
2019-11-14 03:49:01 +0000

    About page added

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,9 @@ Rails.application.routes.draw do
 
   resources :orders, only: [:create, :show]
 
+  
+  get 'about' => 'about#show'
+
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
