14a98e714f726cdfd36c78059e1d58da37fcdf5a
Lizzyfemme <lizzy_swann@hotmail.com>
2019-11-14 02:17:09 +0000

    quantity total added

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,6 +1,28 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["HTTP_ADMIN_USER"],
-                             password: ENV["HTTP_ADMIN_PASSWORD"]
+                               password: ENV["HTTP_ADMIN_PASSWORD"]
   def show
+    @products = Product.order(id: :desc).all
+    @quantity_total = Product.sum(:quantity)
+    
   end
+
+  # def quantity_total
+  #   quantity_total =14
+  #   quantity_total 
+  # end
+
+  def product_params
+    params.require(:product).permit(
+      :name,
+      :description,
+      :category_id,
+      :quantity,
+      :image,
+      :price
+    )
+  end
+ 
+
 end
+
